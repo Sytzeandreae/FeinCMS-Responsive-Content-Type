e3a67e6968af15968b8bde5890aeb13cf9f276e1
Sytze Andreae <sytze@van-ons.nl>
2015-01-13 11:54:24 +0100

    First version of the setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name = 'responsive_content_type',
+    packages = ['responsive_content_type'],
+    version = '0.0.1',
+    description = 'Add responsiveness to the FeinCMS content types',
+    author = 'Sytzeandreae',
+    author_email = 'sytzeandreae@gmail.com',
+    url = 'https://github.com/Sytzeandreae/FeinCMS-Responsive-Content-Type',
+    license = 'GNU-GPL',
+    classifiers = [
+        'Development Status :: ',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU-GPL',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Framework :: FeinCMS',
+        'Topic :: Internet',
+        'Topic :: FeinCMS',
+    ],
+    keywords = 'responsive feincms contenttype content type',
+)
